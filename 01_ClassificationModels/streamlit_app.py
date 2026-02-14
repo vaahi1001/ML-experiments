f00660e77ecd760a442b0c04bfeced6e9721a847
vaahi1001 <vaahika@gmail.com>
2026-02-14 17:51:35 +0530

    Update streamlit_app.py

--- 01_ClassificationModels/streamlit_app.py
+++ 01_ClassificationModels/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import pandas as pd
 
 st.write("Hello World")  # Works inside Streamlit app
