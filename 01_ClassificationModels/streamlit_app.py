97342dbeaa12f4d8b022d04ee2d7873207421104
vaahi1001 <vaahika@gmail.com>
2026-02-15 01:49:07 +0530

    Update streamlit_app.py

--- 01_ClassificationModels/streamlit_app.py
+++ 01_ClassificationModels/streamlit_app.py
@@ -104,6 +104,8 @@ patient_readable['thal'] = patient_readable['thal'].map(thal_map)
 st.write("Summary of Patient Details : ")
 st.dataframe(patient_readable)
 
+models = {}  # initialize as an empty dictionary
+
 # --- Load all model PKLs dynamically ---
 model_files = [
     "/mount/src/ml-experiments/01_ClassificationModels/logisticregression_pipeline.pkl",
@@ -113,12 +115,9 @@ model_files = [
     "/mount/src/ml-experiments/01_ClassificationModels/randomforest_pipeline.pkl",
     "/mount/src/ml-experiments/01_ClassificationModels/xgboost_pipeline.pkl"
 ]
-
-models = {}  # initialize as an empty dictionary
-    
+ 
 # --- Button to run predictions ---
 if st.button("Predict with all models"):
-
     if not models:
         st.error("No models loaded. Check PKL files.")
     else:
